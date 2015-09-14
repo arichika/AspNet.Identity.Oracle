ac8cc7c3974f17095d1208660161da0b0fd85bba
arichika taniguchi <email@arichika.net>
2015-09-15 03:20:37 +0900

    #4 Add alternate processing of "Database.SetInitializer<TContext>".

--- Src/SampleWebSite/App_Start/IdentityConfig.cs
+++ Src/SampleWebSite/App_Start/IdentityConfig.cs
@@ -14,7 +14,6 @@ using AspNet.Identity.Oracle;
 namespace IdentitySample.Models
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
@@ -133,7 +132,7 @@ namespace IdentitySample.Models
 
         public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
         {
-            var manager = new ApplicationRoleManager(new RoleStore<IdentityRole>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationRoleManager(new RoleStore<IdentityRole>(context.Get<ApplicationDbContext>() as OracleDatabase));
 
             return manager;
         }
@@ -157,44 +156,108 @@ namespace IdentitySample.Models
         }
     }
 
-    // This is useful if you do not want to tear down the database each time you run the application.
-    // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
-    // This example shows you how to create a new database if the Model changes
-    //public class ApplicationDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext> {
-    //    protected override void Seed(ApplicationDbContext context) {
-    //        InitializeIdentityForEF(context);
-    //        base.Seed(context);
-    //    }
 
-    //    //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
-    //    public static void InitializeIdentityForEF(ApplicationDbContext db) {
-    //        var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
-    //        var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-    //        const string name = "admin@admin.com";
-    //        const string password = "Admin@123456";
-    //        const string roleName = "Admin";
+     //This is useful if you do not want to tear down the database each time you run the application.
+    public class ApplicationDbInitializer : IDisposable
+    {
+        private static readonly object thisLock = new object();
+        private static ApplicationDbInitializer applicationDbInitializer;
 
-    //        //Create Role Admin if it does not exist
-    //        var role = roleManager.FindByName(roleName);
-    //        if (role == null) {
-    //            role = new IdentityRole(roleName);
-    //            var roleresult = roleManager.Create(role);
-    //        }
+        private bool isInitialized;
+        private ApplicationDbInitializer()
+        {
+        }
 
-    //        var user = userManager.FindByName(name);
-    //        if (user == null) {
-    //            user = new ApplicationUser { UserName = name, Email = name };
-    //            var result = userManager.Create(user, password);
-    //            result = userManager.SetLockoutEnabled(user.Id, false);
-    //        }
+        public static ApplicationDbInitializer Create(IdentityFactoryOptions<ApplicationDbInitializer> options, IOwinContext context)
+        {
+            if (applicationDbInitializer != null)
+                return applicationDbInitializer;
 
-    //        // Add user admin to Role Admin if not already added
-    //        var rolesForUser = userManager.GetRoles(user.Id);
-    //        if (!rolesForUser.Contains(role.Name)) {
-    //            var result = userManager.AddToRole(user.Id, role.Name);
-    //        }
-    //    }
-    //}
+            lock (thisLock)
+            {
+                if (applicationDbInitializer == null)
+                {
+                    applicationDbInitializer = new ApplicationDbInitializer();
+                    applicationDbInitializer.Seed(context);
+                }
+            }
+            return applicationDbInitializer;
+        }
+
+        /// <summary>
+        /// Run Once Initialize code blocks.
+        /// </summary>
+        /// <param name="context"></param>
+        private void Seed(IOwinContext context)
+        {
+            if (isInitialized) return;
+
+            InitializeDb(context);
+            InitializeIdentity(context);
+
+            isInitialized = true;
+        }
+
+        // Verify Db or Tables and Create it, If you need.
+        private void InitializeDb(IOwinContext context)
+        {
+            // var oracleDatabase = context.Get<ApplicationDbContext>() as OracleDatabase;
+
+            // e.g. Run the DDL if Table is not.
+        }
+
+        // Create Sample Admin User: admin@admin.com with password: Admin@123456 in the Admin role
+        private static void InitializeIdentity(IOwinContext context)
+        {
+            var userManager = context.GetUserManager<ApplicationUserManager>();
+            var roleManager = context.Get<ApplicationRoleManager>();
+
+            const string name = "admin@admin.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null) {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null) {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name)) {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+            }
+
+            // Free any unmanaged objects here.
+            disposed = true;
+        }
+    }
 
     public enum SignInStatus
     {
